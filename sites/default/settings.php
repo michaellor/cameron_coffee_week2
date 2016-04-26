3531ee8935a45138f90a9b901d078c2d31ec7d14
eddie duro and michael lor <michael.vang.1985@gmail.com>
2016-04-26 10:09:35 -0700

    complete module for comment buddy

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'camerons_coffee',
       'username' => 'admin',
       'password' => 'password',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
